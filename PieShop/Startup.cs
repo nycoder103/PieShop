2ceaeef71681192cc3463fb53747a77ea540a66f
Rani Zilpelwar <ranizilpelwar@gmail.com>
2017-10-30 22:17:42 -0400

    Create default route to home page

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -60,7 +60,15 @@ namespace PieShop
             app.UseSession();
 
             //Sets up MVC Middleware with default routing schema
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routes => 
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
 
             //Fill database with initial data
             DbInitializer.Seed(app, dbContext);
