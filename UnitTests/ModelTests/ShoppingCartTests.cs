277c342e67e9738ee4f4e69ec02e84d775047287
Rani Zilpelwar <ranizilpelwar@gmail.com>
2017-10-31 22:03:17 -0400

    organize test classes

---
+++ UnitTests/ModelTests/ShoppingCartTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PieShop.Controllers;
+using PieShop.Models;
+using PieShop.ViewModels;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestCategory("Shopping Cart Tests")]
+    [TestClass]
+    public class ShoppingCartTests
+    {
+        ShoppingCart shoppingCart;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            if (shoppingCart == null)
+            {
+                shoppingCart = new ShoppingCart(new AppDbContext(new DbContextOptions<AppDbContext>()));
+
+            }
+        }
+    }
+}
